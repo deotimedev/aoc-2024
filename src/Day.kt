fc2598fa808d069ca10b8916620d5e3a21476814
deotime <89555032+deotimedev@users.noreply.github.com>
2024-12-02 13:24:35 -0600

    update

---
+++ src/Day.kt
@@ -0,0 +1,31 @@
+import kotlin.time.measureTimedValue
+
+interface Day {
+    val day: Int
+    fun part1(input: List<String>): Int
+    fun part2(input: List<String>): Int = TODO("Part 2 not yet available")
+}
+
+fun main() {
+    val day = run {
+        print("Enter day to run: ")
+        readln().toIntOrNull() ?: return System.err.println("Invalid day.")
+    }
+
+    val runner =
+        runCatching { Class.forName("days.Day${day}").getDeclaredField("INSTANCE").get(null) as Day }
+            .getOrNull() ?: return System.err.println("Unable to load day $day, make sure it is defined correctly (days/Day$day.kt).")
+
+    println("Running day $day...")
+
+    val input = readInput("Day${day}")
+    val (result1, time1) = measureTimedValue { runner.part1(input) }
+    println("Part 1: $result1 ($time1)")
+
+    try {
+        val (result2, time2) = measureTimedValue { runner.part2(input) }
+        println("Part 2: $result2 ($time2)")
+    } catch (e: NotImplementedError) {
+        println("Part 2: Not implemented yet")
+    }
+}
---
+++ src/days/Day1.kt
@@ -0,0 +1,11 @@
+package days
+
+import Day
+
+object Day1 : Day {
+    override val day = 1
+
+    override fun part1(input: List<String>): Int {
+        TODO("Not yet implemented")
+    }
+}
